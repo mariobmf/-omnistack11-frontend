caf6f1fd893fcf97c251e1ca5dcae5be01daa855
Mário Fernandes <mariobmf02@gmail.com>
2020-04-14 20:17:57 -0300

    Creation and stylization of the Profile page

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function src() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon} />
-        <Route path="/Register" component={Register} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
