adf01059f552c292f078ff4548f1302af9c21f43
Mário Fernandes <mariobmf02@gmail.com>
2020-04-14 21:49:17 -0300

    Added all page features

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import heroes from '../../assets/heroes.png';
 
 import './styles.css';
 
-export default function Logon() {
+export default function Logon({ history }) {
+  const [id, setId] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      // eslint-disable-next-line no-alert
+      alert('Erro no login, tente novamente');
+    }
+  };
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logo} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
 
-          <input placeholder="Seu ID" />
+          <input
+            placeholder="Seu ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import './styles.css';
 
-export default function NewIncident() {
+export default function NewIncident({ history }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    const ongId = localStorage.getItem('ongId');
+
+    try {
+      await api.post(`/incidents/`, data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      history.push('/profile');
+    } catch (error) {
+      // eslint-disable-next-line no-alert
+      alert('Erro ao cadastrar o caso, tente novamente.');
+    }
+  };
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -25,10 +56,22 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Titulo do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Titulo do caso"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor em reais"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
           <button className="button" type="submit">
             Cadastrar
           </button>
